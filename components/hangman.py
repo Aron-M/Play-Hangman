df34cec91b123c2d617854e6d60cdbf0ecbc5f1f
Aron-M <aron.moelich88@gmail.com>
2023-05-31 10:56:16 +0000

    created function 'get_words' to grab a word in a line from 'words.txt'

--- components/hangman.py
+++ components/hangman.py
@@ -1,2 +1,11 @@
 import random
 
+
+def get_words(filepath='words.txt'):
+    result = []
+    with open(filepath) as n:
+        lines = n.readlines()
+        for line in lines:
+            word = str(line).lower().strip()
+            result.append(word)
+        return result
