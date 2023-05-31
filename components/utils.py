89763af9e6eca1c38f4a3411c71e85d1c51cdd03
Aron-M <aron.moelich88@gmail.com>
2023-05-31 19:09:13 +0000

    WIP started to create 'start_round' function focussing first on logic when player guesses a letter correctly

--- components/utils.py
+++ components/utils.py
@@ -9,18 +9,34 @@ def get_words(filepath='words.txt'):
 
 
 def initialize_game():
-    user = ''
-    while user == '':
-        user = input(("Hello and welcome! Do you fancy a game of hangman? "
-                      "Type 'yes' or 'start' to play. "
-                      "If not, type 'exit' or 'no' to quit")).lower().strip()
-        if user == 'yes' or user == 'start':
-            run_hangman_game()
-            user = ''
-        if user == 'exit' or user == 'no':
+    user_name = input("Hello and welcome! Please enter your name: ")
+    
+    user_choice = ''
+    while user_choice == '':
+        user_choice = input(f"Hello, {user_name}! Do you fancy a game of hangman? "
+                            "Type 'yes' or 'start' to play. "
+                            "If not, type 'exit' or 'no' to quit: ").lower().strip()
+        
+        if user_choice in ['yes', 'start']:
+            run_hangman_game(user_name)
+            user_choice = ''
+        elif user_choice in ['exit', 'no']:
             print('See you next time!')
             exit()
         else:
-            print('Please type y or yes to play, else type n or no to quit.')
-            user = ''
+            print('Please type "yes" or "start" to play, or "exit" or "no" to quit.')
+            user_choice = ''
+
+
+
+def start_round():
+    while tries > 0:
+        output = ''
+        for letter in word:
+            if letter in guessed_correct:
+                output += letter
+        
+        while guess in word:
+            print("That was great! Well done, you guessed the right letter!")
+            guessed_correct.append(guess)
 
