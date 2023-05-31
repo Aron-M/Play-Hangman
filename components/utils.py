529fd2f353c89094c00dda1f65b6400891347553
Aron-M <aron.moelich88@gmail.com>
2023-05-31 17:52:00 +0000

    created 'initialize_hangman' function within 'utils.py' file

--- components/utils.py
+++ components/utils.py
@@ -6,3 +6,18 @@ def get_words(filepath='words.txt'):
             word = str(line).lower().strip()
             result.append(word)
         return result
+
+
+def initialize_game():
+    user = ''
+    while user == '':
+        user = input("Hello and welcome! Do you fancy a game of hangman? Type 'yes'or 'start' to play. If not, type 'exit' or 'no' to quit").lower().strip()
+        if user == 'yes' or user == 'start':
+            run_hangman_game()
+            user = ''
+        if user == 'exit' or user == 'no':
+            print('See you next time!')
+            exit()
+        else:
+            print('Please type y or yes to play, else type n or no to quit.')
+            user = ''
