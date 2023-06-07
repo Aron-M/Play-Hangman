0cdc4648f449a230d3da51566efb922775678981
Aron-M <aron.moelich88@gmail.com>
2023-06-07 11:23:22 +0000

    due to import errors, I included all functions to be on the same page and discarded 'guessed_correct' and 'gessed_incorrect' functions. recreated them to be if statements

--- components/hangman.py
+++ components/hangman.py
@@ -1,36 +1,3 @@
-import random
-from hangman_ascii_art import print_hangman_art
-from utils import get_words, initialize_game, run_hangman_game, guessed_correct, guessed_incorrect
-
+from utils import initialize_game
 
 initialize_game()
-
-def run_hangman_game(user_name):
-    user_name = ''
-    words = get_words()
-    word = random.choice(words).lower().strip()
-    word_progress = ['_'] * len(word)
-    amount_correct = []
-    amount_incorrect = []
-    guesses = 7
-    hangman_count = -1
-
-    while guesses > 0:
-        print_hangman_art(hangman_count)
-        print("Guessed Correct:", amount_correct)
-        print("Guessed Incorrect:", amount_incorrect)
-        print("Tries left:", guesses)
-        print("Word progress:", ' '.join(word_progress))
-
-    guess = input("Enter a letter: ").lower().strip()
-
-    if len(guess) != 1:
-        print("Please enter only one letter.")
-        continue
-
-    if guess in amount_correct or guess in amount_incorrect:
-        print("You have already guessed that letter.")
-        continue
-
-    if guess in word:
-        guessed_correct()
--- components/utils.py
+++ components/utils.py
@@ -1,4 +1,84 @@
-def get_words(filepath='words.txt'):
+import random
+
+
+def print_hangman_art(hangman_count):
+    hangman_image = [
+        """
+        +---+
+          | |
+          O |
+            |
+            |
+            |
+         ======
+        """, 
+
+        """
+        +---+
+          | |
+          O |
+         /  |
+            |
+            |
+         ======
+        """, 
+
+        """
+        +---+
+          | |
+          O |
+         /| |
+            |
+            |
+         ======
+        """, 
+
+        """
+        +---+
+          | |
+          O |
+         /|\|
+            |
+            | 
+         ======
+        """, 
+
+        """
+        +---+
+          | |
+          O |
+         /|\|
+          | | 
+            | 
+         ======
+        """, 
+
+        """
+        +---+
+          | |
+          O |
+         /|\|
+          | | 
+         /  | 
+         ======
+        """, 
+
+        """
+        +---+
+          | |
+          O |
+         /|\|
+          | | 
+         / \| 
+         ======
+        """
+    ]
+
+    print(hangman_image[hangman_count])
+
+
+
+def get_words(filepath='/workspace/Play-Hangman/words.txt'):
     result = []
     with open(filepath) as n:
         lines = n.readlines()
@@ -10,15 +90,15 @@ def get_words(filepath='words.txt'):
 
 def initialize_game():
     user_name = input("Hello and welcome! Please enter your name: ")
-    
+
     user_choice = ''
     while user_choice == '':
-        user_choice = input(f"Hello, {user_name}!"
+        user_choice = input(f"Hello, {user_name}! "
                             "Do you fancy a game of hangman? "
                             "Type 'yes' or 'start' to play. "
-                            "If not, type 'exit'" 
+                            "If not, type 'exit' "
                             "or 'no' to quit: ").lower().strip()
-        
+
         if user_choice in ['yes', 'start']:
             run_hangman_game(user_name)
             user_choice = ''
@@ -26,26 +106,51 @@ def initialize_game():
             print('See you next time!')
             exit()
         else:
-            print('Please type "yes" or "start" to play,'
+            print('Please type "yes" or "start" to play, '
                   'or "exit" or "no" to quit.')
             user_choice = ''
 
 
-def guessed_correct():
-    if letter in word_progress:
-        output += letter
-        
-    while guess in word:
-        print(f"Well done {user_name}, "
-              "you guessed a correct letter!" + '\U0001F44F' * 3)
-        guessed_correct.append(guess)
-        print(word_progress)
-        user_choice = ''
+def run_hangman_game(user_name):
+    words = get_words()
+    word = random.choice(words).lower().strip()
+    word_progress = ['_'] * len(word)
+    guessed_correct = []
+    guessed_incorrect = []
+    guesses = 7
+    hangman_count = -1
 
+    while guesses > 0:
+        print_hangman_art(hangman_count)
+        print("Guessed Correct:", guessed_correct)
+        print("Guessed Incorrect:", guessed_incorrect)
+        print("Tries left:", guesses)
+        print("Word progress:", ' '.join(word_progress))
 
-def guessed_incorrect():
-    amount_incorrect.append(guess)
-    hangman_count += 1
-    guesses -= 1
-    print(f"Sorry {user_name}, you have guessed the wrong letter. "
-          f"Let's go again")
+        guess = input("Enter a letter: ").lower().strip()
+
+        if len(guess) != 1:
+            print("Please enter only one letter.")
+            continue
+
+        if guess in guessed_correct or guess in guessed_incorrect:
+            print("You have already guessed that letter.")
+            continue
+
+        if guess in word:
+            guessed_correct.append(guess)
+            for i in range(len(word)):
+                if word[i] == guess:
+                    word_progress[i] = guess
+            if '_' not in word_progress:
+                print("Congratulations, you guessed the word correctly!")
+                break
+        else:
+            guessed_incorrect.append(guess)
+            hangman_count += 1
+            guesses -= 1
+            print("Wrong guess!")
+
+    if guesses == 0:
+        print_hangman_art(hangman_count)
+        print("Oops! You ran out of tries. The word was:", word)
