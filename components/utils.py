f35a4d97a20e8b22ef2c526a3ece4491e060e96a
Aron-M <aron.moelich88@gmail.com>
2023-06-07 10:22:03 +0000

    creating a 'while' loop to print out the various status point of the game, for amount of letters guessed correctly and incorrectly, tries left and the current word progress. Added 'word_progreaa' variable

--- components/utils.py
+++ components/utils.py
@@ -35,10 +35,18 @@ def run_hangman_game(user_name):
     user_name = ''
     words = get_words()
     word = random.choice(words).lower().strip()
+    word_progress = ['_'] * len(word)
     amount_correct = []
     amount_incorrect = []
     guesses = 7
     hangman_count = -1
+
+    while guesses > 0:
+        print_hangman_art(hangman_count)
+        print("Guessed Correct:", amount_correct)
+        print("Guessed Incorrect:", amount_incorrect)
+        print("Tries left:", guesses)
+        print("Word progress:", ' '.join(word_progress))
 
     guess = input("Enter a letter: ").lower().strip()
 
