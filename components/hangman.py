0a1fc386bb62840834a2e0de2017281807822ec6
Aron-M <aron.moelich88@gmail.com>
2023-06-07 09:34:58 +0000

    changed global variables 'guessed_' to 'amount_' in order to avoid clashes with similaryly named functions

--- components/hangman.py
+++ components/hangman.py
@@ -1,11 +1,12 @@
 import random
 from hangman_ascii_art import print_hangman_art
-from utils import get_words
+from utils import get_words, initialize_game, guessed_correct, guessed_incorrect
+
 
 user_name = ''
 words = get_words()
 word = random.choice(words).lower().strip()
-guessed_correct = []
-guessed_incorrect = []
+amount_correct = []
+amount_incorrect = []
 guesses = 7
 hangman_count = -1
